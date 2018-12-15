3422bb1063579fc936ca0e08e3084f2e5c30a794
Nicolas Maraingel <zerozsaberster2@gmail.com>
2018-12-15 14:05:26 -0300

    Agregar graficos en componentes aparte

---
+++ frontend/src/components/Charts/chart-doughnut.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <h2 class="text-center">Doughnut</h2>
+
+  <div class="card">
+    <chartjs-pie v-bind:labels="labels"
+      v-bind:datasets="datasets"
+      v-bind:option="option"
+      v-bind:bind="true"/>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      labels: ["Gabriel García", "Pablo Coelho", "Pablo Neruda"],
+      datasets: [
+        {
+          data: [100, 20, 500],
+          backgroundColor: ["Blue", "Orange", "Green"]
+        }
+      ],
+      option: {
+        title: {
+          display: true,
+          position: "bottom",
+          text: "Libros"
+        }
+      },
+    };
+  }
+};
+</script>
--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,180 +1,11 @@
 <template>
   <div class="content">
     <div class="md-layout">
-    <chartjs-doughnut v-bind:labels="labels" v-bind:datasets="datasets" v-bind:option="option" :width="300" :height="500"></chartjs-doughnut>
-      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
-        <chart-card
-          :chart-data="dailySalesChart.data"
-          :chart-options="dailySalesChart.options"
-          :chart-type="'Line'"
-          data-background-color="blue">
-          <template slot="content">
-            <h4 class="title">Daily Sales</h4>
-              <p class="category">
-                <span class="text-success"><i class="fas fa-long-arrow-alt-up"></i> 55% </span> increase in today sales.
-              </p>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>access_time</md-icon>
-              updated 4 minutes ago
-            </div>
-          </template>
-        </chart-card>
-      </div>
-      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
-        <chart-card
-          :chart-data="emailsSubscriptionChart.data"
-          :chart-options="emailsSubscriptionChart.options"
-          :chart-responsive-options="emailsSubscriptionChart.responsiveOptions"
-          :chart-type="'Bar'"
-          data-background-color="red">
-          <template slot="content">
-            <h4 class="title">Email Subscription</h4>
-              <p class="category">
-                Last Campaign Performance
-              </p>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>access_time</md-icon>
-              updated 10 days ago
-            </div>
-          </template>
-        </chart-card>
-      </div>
-      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
-        <chart-card
-          :chart-data="dataCompletedTasksChart.data"
-          :chart-options="dataCompletedTasksChart.options"
-          :chart-type="'Line'"
-          data-background-color="green">
-          <template slot="content">
-            <h4 class="title">Completed Tasks</h4>
-              <p class="category">
-                Last Campaign Performance
-              </p>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>access_time</md-icon>
-              campaign sent 26 minutes ago
-            </div>
-          </template>
-        </chart-card>
-      </div>
-      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
-        <stats-card data-background-color="green">
-          <template slot="header">
-            <md-icon >store</md-icon>
-          </template>
-
-          <template slot="content">
-            <p class="category">Revenue</p>
-            <h3 class="title">$34,245</h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-                <md-icon>date_range</md-icon>
-                Last 24 Hours
-            </div>
-          </template>
-        </stats-card>
-      </div>
-      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
-        <stats-card data-background-color="orange">
-          <template slot="header">
-            <md-icon >content_copy</md-icon>
-          </template>
-
-          <template slot="content">
-            <p class="category">Used Space</p>
-            <h3 class="title">49/50
-                <small>GB</small>
-            </h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon class="text-danger">warning</md-icon>
-              <a href="#pablo">Get More Space...</a>
-            </div>
-          </template>
-        </stats-card>
-      </div>
-      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
-        <stats-card data-background-color="red">
-          <template  slot="header">
-            <md-icon >info_outline</md-icon>
-          </template>
-
-          <template slot="content">
-            <p class="category">Fixed Issues</p>
-            <h3 class="title">75</h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-               <md-icon>local_offer</md-icon>
-               Tracked from Github
-            </div>
-          </template>
-        </stats-card>
-      </div>
-      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
-        <stats-card data-background-color="blue">
-          <template  slot="header">
-            <i class="fab fa-twitter"></i>
-          </template>
-
-          <template slot="content">
-            <p class="category">Folowers</p>
-            <h3 class="title">+245</h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-               <md-icon>update</md-icon>
-               Just Updated
-            </div>
-          </template>
-        </stats-card>
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50">
+        <chartjs-line/>
       </div>
       <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50">
-        <md-card>
-          <md-card-header data-background-color="orange">
-            <h4 class="title">Employees Stats</h4>
-            <p class="category">New employees on 15th September, 2016</p>
-          </md-card-header>
-          <md-card-content>
-            <ordered-table table-header-color="orange"></ordered-table>
-          </md-card-content>
-        </md-card>
-      </div>
-      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50">
-        <nav-tabs-card>
-          <template slot="content">
-            <span class="md-nav-tabs-title">Tasks:</span>
-            <md-tabs md-sync-route class="md-success" md-alignment="left">
-
-              <md-tab id="tab-home" md-label="Bugs" md-icon="bug_report">
-                <nav-tabs-table></nav-tabs-table>
-              </md-tab>
-
-              <md-tab id="tab-pages" md-label="Website" md-icon="code">
-                <nav-tabs-table></nav-tabs-table>
-              </md-tab>
-
-              <md-tab id="tab-posts" md-label="server" md-icon="cloud">
-                <nav-tabs-table></nav-tabs-table>
-              </md-tab>
-            </md-tabs>
-          </template>
-        </nav-tabs-card>
+        <chart-doughnut/>
       </div>
     </div>
   </div>
@@ -189,31 +20,19 @@ import {
   OrderedTable
 } from "@/components";
 
+import ChartDoughnut from "@/components/Charts/chart-doughnut";
+
 export default {
   components: {
     StatsCard,
     ChartCard,
     NavTabsCard,
     NavTabsTable,
-    OrderedTable
+    OrderedTable,
+    ChartDoughnut
   },
   data() {
     return {
-      labels: ["Gabriel García", "Pablo Coelho", "Pablo Neruda"],
-      datasets: 
-      [
-        {
-          data: [100, 20, 500],
-          backgroundColor: ["Blue", "Orange", "Green"]
-        }
-      ],
-      option: {
-        title: {
-          display: true,
-          position: "bottom",
-          text: "Libros"
-        }
-      },
       dailySalesChart: {
         data: {
           labels: ["M", "T", "W", "T", "F", "S", "S"],
