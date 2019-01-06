f5b7dbaf39601856ee430dd99b0f665614dd5619
Unknown <zerozsaberster2@gmail.com>
2019-01-06 19:26:54 -0300

    Gráfico de popularidad temporal visible

---
+++ frontend/src/components/Charts/chart-temporal-popularity.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+  <h2 class="text-center" style="padding-bottom:0;">Popularidad de la literatura últimamente</h2>
+
+  <div class="card">
+    <header>
+      <chartjs-line v-bind:beginzero="beginZero"
+        v-bind:labels="labels"
+        v-bind:datalabel="dataLabel"
+        v-bind:data="data"
+        v-bind:option="option"
+        v-bind:bind="true"/>
+    </header>
+  </div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+/*
+  data() {
+    return {
+      beginZero: true,
+      labels: {
+        day: [8, 10, 12, 14, 16],
+        week: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+      },
+      dataLabel: "Foo",
+      radio: "day",
+      data: {
+        day: [1, 3, 5, 3, 1],
+        week: [12, 14, 16, 18, 11, 13, 15]
+      },
+      bgColor: "#ff8a80",
+      borderColor: "#f44336"
+    };
+  }
+};
+*/
+  data() {
+    return {
+      beginZero: true,
+      labels: [],
+      data: [],
+      dataLabel: "Popularidad de libros y autores",
+      option: {
+        title: {
+          display: true/*,
+          position: "bottom",
+          text: "Libros"*/
+        }
+      }
+    };
+  },
+  mounted() {
+    axios({ method: "GET", "url": "http://192.168.0.21:8082/elasticsearch/getDate" }).then(result => {
+      this.labels = result.data.labels;
+      this.data = result.data.data;
+    }, error => {
+      console.error(error);
+    });
+  }
+};
+</script>
--- frontend/src/pages/Tiempo.vue
+++ frontend/src/pages/Tiempo.vue
@@ -2,18 +2,18 @@
   <div class="content">
     <div class="md-layout">
       <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
-        <chart-most-popular-authors/>
+        <chart-temporal-popularity/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ChartMostPopularAuthors from "@/components/Charts/chart-most-popular-authors";
+import chartTemporalPopularity from "@/components/Charts/chart-temporal-popularity";
 
 export default {
   components: {
-    ChartMostPopularAuthors
+    chartTemporalPopularity
   }
 };
 </script>
