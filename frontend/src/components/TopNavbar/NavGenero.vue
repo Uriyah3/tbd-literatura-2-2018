d2fc90e3b3e899b9e95e2dd4c9bef6a3b3525157
Enrique Avilés <enrique.aviles@symbiose.cl>
2019-01-06 19:25:14 -0300

    ADD: Se agregan archivos de las barras cpn botones

---
+++ frontend/src/components/TopNavbar/NavGenero.vue
@@ -0,0 +1,33 @@
+<template>
+  <md-toolbar md-elevation="0" class="md-transparent">
+    <div class="md-toolbar-row">
+      <div class="md-toolbar-section-start">
+        <h3 class="md-title">{{$route.name}}</h3>
+      </div>
+      <div class="md-toolbar-section-end">
+        <md-button class="md-just-icon md-simple md-toolbar-toggle" :class="{toggled: $sidebar.showSidebar}" @click="toggleSidebar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </md-button>
+
+        <div class="md-collapse">
+          
+          <md-list>
+            <md-list-item to="/generos">
+              <i class="material-icons" style="font-size:48px;">pie_chart</i>
+              <p class="hidden-lg hidden-md">Géneros</p>
+              
+            </md-list-item>
+
+            <md-list-item to="/generos">
+              <i class="material-icons" style="font-size:48px;">format_list_numbered</i>
+              <p class="hidden-lg hidden-md">Dashboard</p>
+            </md-list-item>            
+          </md-list>
+        </div>
+      </div>
+    </div>
+
+  </md-toolbar>
+</template>
